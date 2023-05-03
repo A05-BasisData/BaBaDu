e2c7914fc3ef8a7c72e2b3a2b14f08b083cc0665
kristo.jeremy <kristo.jeremy@ui.ac.id>
2023-05-04 03:06:26 +0700

    Tambah frontend buat member pelatih

--- pelatih/urls.py
+++ pelatih/urls.py
@@ -4,6 +4,8 @@ from pelatih.views import *
 app_name = 'pelatih'
 
 urlpatterns = [
+    path('', dashboard_pelatih, name = 'dashboard_pelatih'),
     path('dashboard/', dashboard_pelatih, name = 'dashboard_pelatih'),
-
+    path('daftar_atlet/', daftar_atlet, name = 'daftar_atlet'),
+    path('list_atlet/', list_atlet, name = 'list_atlet'),
 ]
--- pelatih/views.py
+++ pelatih/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def dashboard_pelatih(request):
     return render (request, 'dashboardPelatih.html')
+
+def daftar_atlet(request):
+    return render (request, 'daftarAtlet.html')
+
+def list_atlet(request):
+    return render (request, 'listAtlet.html')
