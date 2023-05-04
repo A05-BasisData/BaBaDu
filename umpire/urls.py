cba9a59142ed9dedba4d0bd92aa0470c7610eea1
kristo.jeremy <kristo.jeremy@ui.ac.id>
2023-05-05 03:57:41 +0700

    Tambah frontend buat member umpire

--- umpire/urls.py
+++ umpire/urls.py
@@ -4,6 +4,9 @@ from umpire.views import *
 app_name = 'umpire'
 
 urlpatterns = [
+    path('', dashboard_umpire, name = 'dashboard_umpire'),
     path('dashboard/', dashboard_umpire, name = 'dashboard_umpire'),
-
+    path('list_event/', lihat_event, name = 'lihat_event'),
+    path('pertandingan/', pertandingan, name = 'pertandingan'),
+    path('hasil_pertandingan/', hasil_pertandingan, name = 'hasil_pertandingan'),
 ]
--- umpire/views.py
+++ umpire/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def dashboard_umpire(request):
     return render (request, 'dashboardUmpire.html')
+
+def lihat_event(request):
+    return render (request, 'listEvent.html')
+
+def pertandingan(request):
+    return render (request, 'pertandingan.html')
+
+def hasil_pertandingan(request):
+    return render (request, 'hasilPertandingan.html')
