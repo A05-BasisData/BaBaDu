440bf7992d0d6b55746085a125642e1829541f1f
elsa.giana <elsagiana18@gmail.com>
2023-05-24 06:57:55 +0700

    add fitur daftar atlet

--- umpire/urls.py
+++ umpire/urls.py
@@ -6,6 +6,7 @@ app_name = 'umpire'
 urlpatterns = [
     path('', dashboard_umpire, name = 'dashboard_umpire'),
     path('dashboard/', dashboard_umpire, name = 'dashboard_umpire'),
+    path('daftar_atlet/', umpire_daftar_atlet, name = 'umpire_daftar_atlet'),
     path('list_event/', lihat_event, name = 'lihat_event'),
     path('pertandingan/<str:prtdg>/', pertandingan, name = 'pertandingan'),
     path('hasil_pertandingan/', hasil_pertandingan, name = 'hasil_pertandingan'),
--- umpire/views.py
+++ umpire/views.py
@@ -4,6 +4,39 @@ from utility.query import query
 def dashboard_umpire(request):
     return render (request, 'dashboardUmpire.html')
 
+def umpire_daftar_atlet(request):
+    atlet_kualifikasi = {}
+    atlet_non_kualifikasi = {}
+    atlet_ganda = {}
+
+    atlet_kualifikasi['atlet_kualifikasi'] = [atlet_kualifikasi._asdict() for atlet_kualifikasi in query(
+        f'''SELECT M.Nama, A.Tgl_Lahir, A.Negara_Asal, A.Play_Right, A.Height, A.World_Rank, AK.World_Tour_Rank, A.Jenis_Kelamin, PH.Total_Point
+        FROM MEMBER M JOIN ATLET A ON M.ID = A.ID JOIN ATLET_KUALIFIKASI AK ON A.ID = AK.ID_Atlet 
+        INNER JOIN (SELECT ID_Atlet, MAX(Total_Point) AS Total_Point FROM POINT_HISTORY GROUP BY ID_Atlet) PH ON A.ID = PH.ID_Atlet;
+        '''
+    )]
+
+    atlet_non_kualifikasi['atlet_non_kualifikasi'] = [atlet_non_kualifikasi._asdict() for atlet_non_kualifikasi in query(
+        f'''SELECT M.Nama, A.Tgl_Lahir, A.Negara_Asal, A.Play_Right, A.Height, A.World_Rank, A.Jenis_Kelamin
+        FROM MEMBER M JOIN ATLET A ON M.ID = A.ID JOIN ATLET_NON_KUALIFIKASI ANK ON A.ID = ANK.ID_Atlet;
+        '''
+    )]
+
+    # atlet_ganda['atlet_ganda'] = [atlet_ganda._asdict() for atlet_ganda in query(
+    #     f'''SELECT ID_Atlet_Ganda, AK1.Nama, AK2.Nama
+    #     FROM ATLET_GANDA AG JOIN ATLET_KUALIFIKASI AK1, AK2 ON (AG.ID_Atlet_Kualifikasi = AK1.ID_ATLET OR AG.ID_Atlet_Kualifikasi = AK2.ID_ATLET);
+    #     '''
+    # )]
+    # print(atlet_ganda)
+
+    context = {
+        'data_ak':atlet_kualifikasi,
+        'data_ank':atlet_non_kualifikasi,
+        'data_ag':atlet_ganda
+    }
+
+    return render (request, 'umpireDaftarAtlet.html', context)
+
 def lihat_event(request):
     event = {}
     event['event'] = [event._asdict() for event in query(
